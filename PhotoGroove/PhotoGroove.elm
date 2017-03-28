45194d4849212a82749cc97947bc5821ae48cae2
claudiordgz <claudio.rdgz@gmail.com>
2017-03-27 23:30:30 -0400

    WIP: chapter 02

---
+++ PhotoGroove/PhotoGroove.elm
@@ -0,0 +1,41 @@
+module PhotoGroove exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+urlPrefix = 
+  "http://elm-in-action.com/"
+
+initialModel = 
+  { photos =
+      [ { url = "1.jpeg" }
+      , { url = "2.jpeg" }
+      , { url = "3.jpeg" }
+      ]
+    , selectedUrl = "1.jpeg"
+  }
+
+view model =
+    div [ class "content" ]
+        [ h1 [] [ text "Photo Groove" ]
+        , div [ id "thumbnails" ] 
+            (List.map (viewThumbnail model.selectedUrl) 
+              model.photos
+            )
+        , img 
+            [ class "large"
+            , src (urlPrefix ++ "large/" ++ model.selectedUrl)
+            ]
+            []
+        ] 
+        
+viewThumbnail selectedUrl thumbnail = 
+ img 
+   [ src (urlPrefix ++ thumbnail.url) 
+   , classList [ ("selected", selectedUrl == thumbnail.url) ]
+   ] 
+   []
+
+
+        
+main = view initialModel
