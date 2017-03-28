4d90c0cb8b2ec506bdba597eea89cdfd3fe12239
claudiordgz <claudio.rdgz@gmail.com>
2017-03-28 17:29:35 -0400

    WIP: Chapter 3

--- PhotoGroove/PhotoGroove.elm
+++ PhotoGroove/PhotoGroove.elm
@@ -1,11 +1,32 @@
 module PhotoGroove exposing (..)
 
+import Html.App exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
 
+import Array exposing (Array)
+
+urlPrefix : String
 urlPrefix = 
   "http://elm-in-action.com/"
 
+type alias Photo =
+    { url : String }
+
+
+type alias Model =
+    {
+      photos : List Photo
+    , selectedUrl : String
+    }
+
+photoArray : Array Photo
+photoArray =
+    Array.fromList initialModel.photos
+
+
+initialModel : Model
 initialModel = 
   { photos =
       [ { url = "1.jpeg" }
@@ -15,13 +36,23 @@ initialModel =
     , selectedUrl = "1.jpeg"
   }
 
+
+update msg model =
+    if msg.operation == "SELECT_PHOTO" then
+        { model | selectedUrl = msg.data }
+    else
+        model
+
+type alias Msg =
+    { operation : String, data : String }
+
+
+view : Model -> Html Msg
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
         , div [ id "thumbnails" ] 
-            (List.map (viewThumbnail model.selectedUrl) 
-              model.photos
-            )
+            (List.map (viewThumbnail model.selectedUrl)  model.photos)
         , img 
             [ class "large"
             , src (urlPrefix ++ "large/" ++ model.selectedUrl)
@@ -30,12 +61,18 @@ view model =
         ] 
         
 viewThumbnail selectedUrl thumbnail = 
- img 
-   [ src (urlPrefix ++ thumbnail.url) 
+ img
+   [ src (urlPrefix ++ thumbnail.url)
    , classList [ ("selected", selectedUrl == thumbnail.url) ]
-   ] 
+   , onClick { operation = "SELECT_PHOTO", data = thumbnail.url }
+   ]
    []
-
+ 
 
         
-main = view initialModel
+main =
+    Html.App.beginnerProgram
+        { model = initialModel
+        , view = view
+        , update = update
+        }
